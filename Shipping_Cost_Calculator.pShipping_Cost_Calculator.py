cfd9e9cca19b127c89cde6919a5674dac7fa86b5
AsipheCTYWY <s225265982@mandela.ac.za>
2026-08-04 08:42:00 -0400

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.pShipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.pShipping_Cost_Calculator.py
@@ -1,4 +1,4 @@
-# Shipping Cost Calculator
+O# Shipping Cost Calculator
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
@@ -10,4 +10,13 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-# Here is a new update by AsipheCTYWY
+
+
+
+
+
+
+
+
+
+
